a9d0931e6d285f3e8c7b7d5a87bdeda3eb8546af
SADIQ MUSTAPHA AHMAD <sadiqmustaphaahmad@gmail.com>
2023-04-05 03:09:01 -0700

    table not bold anymore

--- resources/views/components/teacher-view.blade.php
+++ resources/views/components/teacher-view.blade.php
@@ -24,8 +24,8 @@
                         @foreach ($teacher->students as $student)
                        
                         <tr  class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap mb-10 lg:mb-0">
-                            <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static font-bold">{{ $student->id }}</td>
-                            <td class="w-full lg:w-auto p-3 text-gray-800 border border-b block lg:table-cell relative lg:static font-bold">{{ $student->fullname }}</td>
+                            <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">{{ $student->id }}</td>
+                            <td class="w-full lg:w-auto p-3 text-gray-800 border border-b block lg:table-cell relative lg:static">{{ $student->fullname }}</td>
                         </tr>
                         @endforeach
                     </tbody>
