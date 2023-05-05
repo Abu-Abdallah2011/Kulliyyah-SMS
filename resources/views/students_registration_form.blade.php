65bbaca7bd0a6ef6eff028fb54dc69987f00c594
SADIQ MUSTAPHA AHMAD <sadiqmustaphaahmad@gmail.com>
2023-05-05 16:31:26 -0700

    bold

--- resources/views/students_registration_form.blade.php
+++ resources/views/students_registration_form.blade.php
@@ -32,7 +32,7 @@
                             </div>
                             {{-- Set --}}
                             <div>
-                                <x-input-label class="font-bold" for="set" :value="__('Set')" />
+                                <x-input-label for="set" :value="__('Set')" />
                                 <select id="set" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" type="text" name="set" required>
                                 <option></option>
                                 <option>H11M</option>
