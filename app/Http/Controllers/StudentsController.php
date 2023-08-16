704cf2aa28b955f8f4f4a4334a8d2782b29971b1
SADIQ MUSTAPHA AHMAD <sadiqmustaphaahmad@gmail.com>
2023-08-15 22:37:19 -0700

    dd

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -84,6 +84,13 @@ public function update(StudentFormRequest $request, $id){
         
     $data = $request->validated();
 
+    dd([
+        'driver' => 's3',
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION'),
+        'bucket' => env('AWS_BUCKET'),]);
+
     $selectedOptionId = $request->input('dynamic_select');
     $selectedOption = sets::find($selectedOptionId);
 
