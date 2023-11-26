ffc5272a8e8db2bc619c82668b618b90f158d9a2
SADIQ MUSTAPHA AHMAD <sadiqmustaphaahmad@gmail.com>
2023-11-25 19:09:53 -0800

    exams005

--- app/Http/Requests/subjectsFormRequest.php
+++ app/Http/Requests/subjectsFormRequest.php
@@ -27,7 +27,7 @@ class subjectsFormRequest extends FormRequest
             'subject' => [
                 'max:255'
             ],
-            'marks_obtainable' => [
+            'category' => [
                 'max:255'
             ],
         ];
