0db0a51669af8937726a80e99b7ebe585f2c5fcb
SADIQ MUSTAPHA AHMAD <sadiqmustaphaahmad@gmail.com>
2024-03-06 08:51:27 -0800

    exams for admin

--- app/Http/Controllers/ExamsController.php
+++ app/Http/Controllers/ExamsController.php
@@ -35,10 +35,6 @@ public function show(){
 
 foreach ($teacher->students as $student) {
     $totalCa[$student->id] = [];
-    $matchingSubjects = $student->exams->where('session', $sessions->session)
-    ->where('term', $sessions->term)
-    ->where('student_id', $student->id);
-    // ->get();
 
     $attendanceRecords[$student->id] = $student->attendance
     ->where('session', $sessions->session)
