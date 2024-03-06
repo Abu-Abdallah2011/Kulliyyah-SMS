1bb8869e61e9a7303587d68326cd105179ef4efb
SADIQ MUSTAPHA AHMAD <sadiqmustaphaahmad@gmail.com>
2024-03-05 20:23:07 -0800

    exams for terms

--- resources/views/Exams/cleansheet.blade.php
+++ resources/views/Exams/cleansheet.blade.php
@@ -56,11 +56,11 @@
 
 
                                 @if (!in_array($subject->subject_id, $subjectsDisplayed))
-                                @if ($matchingSubjects)
+                                {{-- @if ($matchingSubjects) --}}
                                 <th colspan="2" class="text-center font-bold uppercase bg-gray-200  p-2 md:p-3 lg:p-4 text-gray-600 border border-gray-300 lg:table-cell">
                                     <a href="{{ url('/exams/' . $subject->subject_id . '/examsEdit')}}">{{$subject->subject_id}}</a>
                                 </th>
-                                @endif
+                                {{-- @endif --}}
                                 @php
                              $subjectsDisplayed[] = $subject->subject_id;
                          @endphp
@@ -100,13 +100,13 @@
                                     @endphp
 
                                     @if (!in_array($subjects->subject_id, $columnsDisplayed))
-                                    @if ($matchingSubjects)
+                                    {{-- @if ($matchingSubjects) --}}
                                     <td class="w-full lg:w-auto p-3 text-gray-800 border border-b lg:table-cell relative lg:static">{{ $totalCa[$student->id][$subjects->subject_id] }}</td>
                                     <td class="w-full lg:w-auto p-3 text-gray-800 border border-b lg:table-cell relative lg:static">
                                         @foreach($subjective as $exam)
                                         {{ $exam->exams }}</td>
                                         @endforeach
-                                    @endif
+                                    {{-- @endif --}}
                                     @php
                              $columnsDisplayed[] = $subjects->subject_id;
                          @endphp
@@ -128,7 +128,7 @@
                                 </td>
                                     <td class="w-full lg:w-auto p-3 text-gray-800 border border-b lg:table-cell relative lg:static">{{ $student->position}}</td>
                                     <td class="w-full lg:w-auto p-3 text-gray-800 border border-b lg:table-cell relative lg:static">
-                                        @if ($matchingSubjects)
+                                        {{-- @if ($matchingSubjects) --}}
                                         @foreach($matchingSubjects as $subjects)
                                         @php
                                         $subjective = $subjects
@@ -142,7 +142,7 @@
                                         {{ $subject->comment }}
                                         @endforeach
                                         @endforeach
-                                        @endif
+                                        {{-- @endif --}}
                                 </td>
                                     
                                   </tr>
