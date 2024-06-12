6c7b760e6244bf9cfc2516d6111c342f7680db0f
SADIQ MUSTAPHA AHMAD <sadiqmustaphaahmad@gmail.com>
2024-06-12 02:57:01 -0700

    term select corretion

--- resources/views/Sessions/EditSessions.blade.php
+++ resources/views/Sessions/EditSessions.blade.php
@@ -32,6 +32,7 @@
                             <div>
                                 <x-input-label for="term" :value="__('Term')" />
                                 <select id="term" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" type="text" name="term" value="{{$session->term}}" required autofocus autocomplete="term" />
+                                    <option value="{{$session->term}}">{{$session->term}}</option>
                                     <option value="1st Term">1st Term</option>
                                     <option value="2nd Term">2nd Term</option>
                                     <option value="3rd Term">3rd Term</option>
