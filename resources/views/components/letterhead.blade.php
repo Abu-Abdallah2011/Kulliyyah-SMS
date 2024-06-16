9309a7c07d88d04af5ad61ff641b7fc3f4fe9c7d
SADIQ MUSTAPHA AHMAD <sadiqmustaphaahmad@gmail.com>
2024-06-16 14:28:38 -0700

    Add Letterhead and the School Fees Modifications

---
+++ resources/views/components/letterhead.blade.php
@@ -0,0 +1,4 @@
+<h1 class="font-bold text-2xl font-serif text-gray-900 dark:text-white">KULLIYYATU MASJIDIL QUR'AN</h1>
+<p class="font-serif">Makama New Extension, Federal Low-Cost, Bauchi, Bauchi State.</p>
+<p class="font-serif"><span class="font-bold">GSM:</span> 08032116211, 08135966693, 08037917173</p>
+<p class="font-serif"><span class="font-bold">Motto:</span> خيركم من تعلم القرءان وعلمه</p>
