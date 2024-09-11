088abab89534ea9afc2d6295e6fee4eb04972820
SADIQ MUSTAPHA AHMAD <sadiqmustaphaahmad@gmail.com>
2024-09-11 11:15:06 -0700

    display the logged in users' photo at the top or the school badge if he does not have one

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,10 +7,10 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         {{-- <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" /> --}}
-                        <?php
+                        @php
                         $teacher = register_teacher::where('id', Auth::user()->id)->first();
                         $teacherPhoto = $teacher->photo;
-                        ?>
+                        @endphp
                         @if ($teacherPhoto)
                         <div class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200">{{ $teacherPhoto }}</div>
                         @else
